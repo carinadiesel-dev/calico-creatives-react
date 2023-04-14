2a5cf54aa61243fcbcb0904fb774c13da95ea4b3
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-04-14 13:00:06 +0200

    AOC logo added as svg #10

--- src/ui/logos/Logos.stories.tsx
+++ src/ui/logos/Logos.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import {CCLogo} from '@/ui/logos/CCLogo'
+import {CMELogo} from '@/ui/logos/CMELogo'
 
 export default {
   title: 'Components/Logos',
@@ -23,3 +24,20 @@ CCLogoLarge.args = {
   width: 1024,
   height: 600
 }
+
+const CMELogoTemplate: ComponentStory<typeof CMELogo> = args => <CMELogo {...args} />;
+
+export const CMELogoNormal = CMELogoTemplate.bind({});
+CMELogo.args = {
+  width: 911.491,
+  height: 591.898
+}
+
+const AOCLogoTemplate: ComponentStory<typeof AOCLogo> = args => <AOCLogo {...args} />;
+
+export const AOCLogoNormal = AOCLogoTemplate.bind({});
+AOCLogo.args = {
+  width: 1185.479,
+  height: 614.711
+}
+
