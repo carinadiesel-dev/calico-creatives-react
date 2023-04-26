bb3f9c9237b9dc470b5b8f620c9608e1fe8ffcea
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-04-26 10:45:56 +0200

    Navigation Menu sub menu styled #1

--- src/ui/navigation/NavigationMenuBar.tsx
+++ src/ui/navigation/NavigationMenuBar.tsx
@@ -3,6 +3,7 @@ import React, {AnchorHTMLAttributes, FunctionComponent, PropsWithChildren, useSt
 import { CaretDownIcon } from '@radix-ui/react-icons';
 import {CCLogo} from '@/ui/logos/CCLogo';
 import Link from 'next/link';
+import { cn } from '@/utils/cn';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -15,6 +16,44 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import { navigationMenuTriggerStyle, } from "@/components/ui/navigation-menu";
+
+const components: { title: string; href: string; description?: string }[] = [
+  {
+    title: "Logo Design",
+    href: "#",
+    description:
+      "We create professional logos from scratch based on your needs and preferences",
+  },
+  {
+    title: "Animated Graphics",
+    href: "#",
+    description:
+      "To bring a uniqueness to your logo, we can add some motion.",
+  },
+  {
+    title: "Web Design",
+    href: "#",
+    description:
+      "You are currently viewing one of our websites.",
+  },
+  {
+    title: "Branding & Social",
+    href: "#",
+    description: "Social media increases the amount of exposure a brand receives and increases traffic",
+  },
+  {
+    title: "Stationary'",
+    href: "#",
+    description:
+      "We offer luxury stationery for all of life’s special celebrations and events.",
+  },
+  {
+    title: "Poster & Ads",
+    href: "#",
+    description:
+      "Turn anything into a poster",
+  },
+]
 
 // type NavItemButton = {
 //   title: string,
@@ -189,13 +228,18 @@ return (
 <NavigationMenuItem>
   <NavigationMenuItem>
       <NavigationMenuTrigger>Graphic Design</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <NavigationMenuLink>Logo Design</NavigationMenuLink>
-        <NavigationMenuLink>Animated Graphics</NavigationMenuLink>
-        <NavigationMenuLink>Web Design</NavigationMenuLink>
-        <NavigationMenuLink>Branding & Social</NavigationMenuLink>
-        <NavigationMenuLink>Stationary</NavigationMenuLink>
-        <NavigationMenuLink>Posters & Ads</NavigationMenuLink>
+      <NavigationMenuContent className='bg-calicoPink-100'>
+        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+        {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+        </ul>
       </NavigationMenuContent>
     </NavigationMenuItem>
 </NavigationMenuItem>
@@ -219,3 +263,46 @@ return (
 </NavigationMenu>
 )
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors text-[15px]hover:ring-calicoRedOrange-400 hover:ring-2",
+            className
+          )}
+          {...props}
+        >
+          <div className="font-bold leading-none text-md text-calicoRedOrange-400">{title}</div>
+          <p className="text-sm leading-snug line-clamp-2 text-muted-foreground text-calicoGray-400">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
+
+{/* <NavigationMenuItem>
+          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem> */}
