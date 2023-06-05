9142943a14075dc9c80cfb3df82bfcb2e07c8154
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-06-05 14:37:37 +0200

    Added stationary and events page

--- src/pages/graphic-design/stationary.tsx
+++ src/pages/graphic-design/stationary.tsx
@@ -1,4 +1,8 @@
 import { PageTitle } from "@/ui/pageTitle/PageTitle"
+import { Paragraph } from "@/ui/paragraph/Paragraph"
+import { StationaryGallery } from "@/ui/stationary/StationaryGallery"
+import { Button } from "@/components/ui/button"
+import variants from "@/ui/buttons/variants"
 
 type StationaryProps = {
     title: string
@@ -10,6 +14,11 @@ export default function Stationary(){
     return (
         <div>
             <PageTitle title="Stationary & Events" backgroundGradient="bg-gradient-to-r from-calicoWhite-300 to-calicoPeach-300" titleColor="text-calicoPink-400" />
+            <Paragraph text={`We offer luxury stationery for all of life’s special celebrations and events. Birthdays, Weddings, Corporate, Baby Showers`} />
+            <StationaryGallery />
+            <div className="py-28">
+                <Button variant={"peach"}>Get a Quote</Button>
+            </div>
         </div>
     )
 }
