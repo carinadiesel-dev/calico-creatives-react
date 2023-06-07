d12e35b0c1f08061efc556c18e832d7318b2890c
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-06-07 11:01:11 +0200

    Hero responsive changes made

--- src/ui/home/Hero.tsx
+++ src/ui/home/Hero.tsx
@@ -39,16 +39,19 @@ export const Hero = () => {
     //         </div>
     //     </div>
 
-<div className="flex flex-col overflow-hidden bg-center bg-no-repeat bg-contain relative lg:flex-row bg-homeGradient lg:h-[35rem] xl:h-[40rem] 2xl:h-[50rem] items-center w-screen">
-<div className='hidden lg:visible lg:w-[60%] lg:flex lg:translate-x-14 lg:translate-y-24 2xl:translate-x-16 2xl:translate-y-36'>
-    <CCLogo width={logoSize[0]} height={logoSize[1]}/>
+<div className="relative flex flex-col items-center w-screen overflow-hidden bg-center bg-no-repeat bg-contain lg:flex-row bg-homeGradient">
+<div className='hidden lg:visible lg:w-[60%] lg:flex lg:translate-x-14 lg:translate-y-24 2xl:translate-x-48 2xl:translate-y-36'>
+    <CCLogo/>
 </div>
 
-<div className='flex flex-1 lg:absolute lg:-right-7 lg:overflow-hidden lg:w-[35rem] xl:w-[40rem] 2xl:w-[50rem]'>
-<Image     
+<div className='relative flex flex-1'>
+  <div className='lg:overflow-hidden'>
+  <Image     
 src={heroLarge}
 alt="Picture of the founder"
 />
+  </div>
+
 </div>
 
 </div>
