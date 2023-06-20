62b177078dcfc3fa32e87e5cc74073ea95f904b8
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-06-20 11:25:17 +0200

    Video size adjusted on Animated graphics page #14

--- src/pages/graphic-design/animated-graphics.tsx
+++ src/pages/graphic-design/animated-graphics.tsx
@@ -8,11 +8,16 @@ export default function AnimatedGraphics(){
         <div>
             <PageTitle title="Animated Graphics" backgroundGradient="bg-gradient-to-r from-calicoWhite-300 to-calicoPeach-300" titleColor="text-calicoPink-400" />
             <Paragraph text="Animate your logo or create a flashy giff for your company! To bring a uniqueness to your logo, we can add some motion. An animated logo allows the imagination to go out of control. Previous work:"/>
-            <div className="grid h-full gap-12 place-items-center">               
-                <video controls controlsList="nodownload noremoteplayback noplaybackrate foobar" autoPlay>
+            <div className="grid px-5 place-items-center md:px-12 lg:px-16 xl:px-20 2xl:px-28">
+                
+                <video className="w-[90vw] md:w-[75vw] xl:w-[60vw]"controls controlsList="nodownload noremoteplayback noplaybackrate foobar" autoPlay>
                     <source src={`require('../../CME-animated-logo.mp4')`} type="video/mp4"/>
                 </video>
+                             
+                <div className="py-28">
                 <Button variant={'peach'}>Get a Quote</Button>
+                </div>
+                
             </div>
         </div>
     )
