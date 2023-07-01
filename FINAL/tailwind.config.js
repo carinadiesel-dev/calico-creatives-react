781e0f23f45ed109f68662c3581d8d853f8c1e71
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-07-01 11:41:35 +0200

    Added FINAL folder for updated Shadcn UI

---
+++ FINAL/tailwind.config.js
@@ -0,0 +1,100 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        calicoPink: {
+          '400': '#F48987',
+          '200': '#FACCCB',
+          '100': '#FFDAD965'
+        },
+        calicoPeach: {
+          '400': '#F98678',
+          '300': '#F5A892'
+        },
+        calicoOrange: {
+          '400': '#F5AC6D'
+        },
+        calicoRedOrange: {
+          '400': '#F75742'
+        },
+        calicoGray: {
+          '400': '#707070',
+          '200': '#A8A3A3'
+        },
+        calicoWhite: {
+          '400': '#FFFFFF',
+          '300': '#FFFFFF88',
+          '200': '#FFFFFF4D',
+          '100': '#FFFFFF91'
+  
+        },
+      },
+      fontFamily: {
+        'brilon': 'BrilonRegular',
+        sans: [
+          '"Helvetica Neue"',
+          'Roboto',
+          'Arial',
+          'ClashDisplay',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+         
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      backgroundImage:{
+        'cloud': 'url(public/images/vectorimages/cloud.png)',
+
+        'homeGradient': "linear-gradient(to right, rgba(245, 168, 146, 1), rgba(255, 255, 255, 1)), url(public/images/vectorimages/cloud.png)",
+
+        'heroImg': 'url(public/images/hero-images/HeroImg.png)',
+
+        'single' : 'linear-gradient(to right, rgba(255, 255, 255, 0.53), rgba(245, 168, 146, 1)), url(public/images/vectorimages/cloud.png)',
+
+        'photography': 'url(public/images/vectorimages/Photography.png)',
+
+        'graphicDesign': 'url(public/images/vectorimages/GraphicDesign.png)',
+
+        'travel': 'url(public/images/vectorimages/Travel.png)'
+      }
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
