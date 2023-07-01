689ffebda30e2b603c764de88ee650b90c410443
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-07-01 11:47:17 +0200

    Added Custom Fonts back

--- FINAL/tailwind.config.js
+++ FINAL/tailwind.config.js
@@ -50,7 +50,6 @@ module.exports = {
           '"Helvetica Neue"',
           'Roboto',
           'Arial',
-          'ClashDisplay',
           'ui-sans-serif',
           'system-ui',
           '-apple-system',
