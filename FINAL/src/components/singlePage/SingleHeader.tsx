6ec402394d72d6edff3a8496d16c2769c04ca9c0
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-07-14 14:54:37 +0200

    Footer hover removed

--- FINAL/src/components/singlePage/SingleHeader.tsx
+++ FINAL/src/components/singlePage/SingleHeader.tsx
@@ -5,7 +5,7 @@ type SingleHeaderProps = {
 
 export const SingleHeader = ({title}: SingleHeaderProps) => {
     return (
-      <div className="py-16 pl-5 lg:py-20">
+      <div className="py-16 lg:py-20">
         <h2 className="text-4xl md:text-6xl font-brilon lg:text-6xl xl:text-8xl text-calicoPeach-400">
         {title}
         </h2> 
