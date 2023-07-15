b302c5bffdf9e6e3d64f69580b0c97296525f815
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-07-15 11:21:57 +0200

    Removed unused icons

--- FINAL/src/components/icons/Icon.tsx
+++ FINAL/src/components/icons/Icon.tsx
@@ -1,26 +1,4 @@
 import React, {SVGProps} from 'react';
-import {IconGrid} from './IconGrid'
-import {IconTaskSquare} from './IconTaskSquare'
-import {IconInbox} from './IconInbox'
-import {IconChat} from './IconChat'
-import {IconTeam} from './IconTeam'
-import {IconSettings} from './IconSettings'
-import {IconHeadphone} from './IconHeadphone'
-import {IconLock} from './IconLock'
-import {IconBookmark2} from "./IconBookmark2"
-import {IconCalendarEdit} from './IconCalendarEdit'
-import {IconHourglass} from './IconHourglass'
-import {IconCheckmarkCircle} from './IconCheckmarkCircle'
-import {IconXCircle} from './IconXCircle'
-import {IconBlock} from './IconBlock'
-import {IconBookstack} from './IconBookstack'
-import {IconCheck} from './IconCheck'
-import {IconGlobe} from './IconGlobe'
-import {IconHand} from './IconHand'
-import {IconHeart} from './IconHeart'
-import {IconLightning} from './IconLightning'
-import {IconPointer} from './IconPointer'
-import {IconSun} from './IconSun'
 import {IconEmail} from './IconEmail';
 import {IconWeb} from './IconWeb';
 import {IconFacebook} from './IconFacebook';
@@ -29,36 +7,11 @@ import {IconWhatsapp} from './IconWhatsapp';
 import {IconCloud} from './IconCloud'
 import {IconCircleFilled} from './IconCircleFilled'
 import { IconOvalFilled } from './IconOvalFilled';
-import {IconPhotography} from './IconPhotography'
-import {IconGraphicDesign} from './IconGraphicDesign'
-import {IconTravel} from './IconTravel'
 import { IconHamburger } from './IconHamburger';
-import { IconEnvelope } from './IconEnvelope';
 import {IconPlay} from "./IconPlay";
+import { IconEnvelope } from './IconEnvelope';
 
 const _icons = {
-  "grid": IconGrid,
-  "task-square": IconTaskSquare,
-  "inbox": IconInbox,
-  "chat": IconChat,
-  "team": IconTeam,
-  "settings": IconSettings,
-  "headphone": IconHeadphone,
-  "lock": IconLock,
-  "bookmark-2": IconBookmark2,
-  "calendar-edit": IconCalendarEdit,
-  "hourglass": IconHourglass,
-  "checkmark-circle": IconCheckmarkCircle,
-  "x-circle": IconXCircle,
-  "block": IconBlock,
-  "bookstack": IconBookstack,
-  "check": IconCheck,
-  "globe": IconGlobe,
-  "hand": IconHand,
-  "heart": IconHeart,
-  "lightning": IconLightning,
-  "pointer": IconPointer,
-  "sun": IconSun,
   "email": IconEmail,
   "web": IconWeb,
   "facebook": IconFacebook,
@@ -67,12 +20,8 @@ const _icons = {
   "cloud": IconCloud,
   "circleFilled": IconCircleFilled,
   "ovalFilled": IconOvalFilled,
-  "photography": IconPhotography,
-  "graphicDesign": IconGraphicDesign,
-  "travel": IconTravel,
   "hamburger": IconHamburger,
-  "envelope": IconEnvelope,
   "play" : IconPlay,
-
+  "envelope" : IconEnvelope
 }
 
