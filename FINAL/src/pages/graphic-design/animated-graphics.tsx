57c0214a2d959d70ded03346f5f7c5b7816ab552
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-07-29 15:23:37 +0200

    Changed video to gif

--- FINAL/src/pages/graphic-design/animated-graphics.tsx
+++ FINAL/src/pages/graphic-design/animated-graphics.tsx
@@ -8,8 +8,9 @@ import { IconPlay } from "@/components/icons/IconPlay";
 import {Link, Route, Routes} from 'react-router-dom';
 import { useEffect, useState, useRef } from "react";
 import ReactPlayer from 'react-player'
+import gif from "@/assets/CME-animated-logo.gif"
 
-
+const videoClasses = "h-auto aspect-video"
 
 export default function AnimatedGraphics(){
     const [isPlaying, setIsPlaying] = useState(false);
@@ -33,18 +34,11 @@ export default function AnimatedGraphics(){
             <PageTitle title="Animated Graphics" backgroundGradient="bg-gradient-to-r from-calicoWhite-300 to-calicoPeach-300" titleColor="text-calicoPink-400" />
             <Paragraph text="Animate your logo or create a flashy giff for your company! To bring a uniqueness to your logo, we can add some motion. An animated logo allows the imagination to go out of control. Previous work:"/>
             <div className="relative grid px-5 place-items-center md:px-12 lg:px-16 xl:px-20 2xl:px-28">
-                
-                {/* <video className="w-[90vw] md:w-[75vw] xl:w-[60vw]" ref={videoRef} >
-                    <source src={video} type="video/mp4"  />
-                </video> */}
-                <ReactPlayer url={video} playing={isPlaying} onEnded={handleToggle}
-            />
-            
-            <div className={`absolute ${showButtonClasses}`}>
+
+                <img src={gif} className="w-[60rem]"/>
+                <div className={`absolute ${showButtonClasses}`}>
                 <button onClick={handleToggle}><IconPlay/></button>
-                </div>
-                
-                    
+                </div>    
             </div>
             <div className="py-28 xl:mb-16">
                 <Link to="/contact"><CustomButton variant={'peach'} buttonText="Get a Quote"/></Link>
