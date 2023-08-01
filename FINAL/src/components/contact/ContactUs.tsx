bd3e9c9bc459c5dafb8c6fe1348540846fafec77
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-08-01 10:00:12 +0200

    Update contact page

--- FINAL/src/components/contact/ContactUs.tsx
+++ FINAL/src/components/contact/ContactUs.tsx
@@ -5,16 +5,16 @@ import { ContactForm } from "./ContactForm"
 
 export const ContactUs = () => {
     return (
-      <div className="flex flex-col-reverse gap-8 px-16 lg:justify-around lg:flex-row">
+      <div className="flex flex-col-reverse gap-8 px-16 py-16 lg:justify-around lg:flex-row">
         
         <div className="">
          <SingleHeader title="Contact Us" />
-         <ol className="space-y-6 font-sans text-4xl text-calicoGray-400">
+         <ol className="flex flex-col items-center space-y-6 font-sans text-4xl text-calicoGray-400">
             <li>Suané Erweë</li>
             <li className="flex items-center gap-2"><IconEnvelope stroke="#707070" size={1.5} /><a href="#">info@calicocreatives.net</a></li>
          </ol>
         </div>
-        <div className="lg:w-1/3">
+        <div className="lg:w-[38rem]">
           <ContactForm />
         </div>
         
