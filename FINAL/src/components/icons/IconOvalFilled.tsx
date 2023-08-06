4cace68fdad81a0ac6dd6f08fe0f167bc01d75ef
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-08-06 10:40:16 +0200

    Fixed Photography page

--- FINAL/src/components/icons/IconOvalFilled.tsx
+++ FINAL/src/components/icons/IconOvalFilled.tsx
@@ -1,9 +1,9 @@
 import React, {SVGProps} from 'react'
 
-type IconOvalFilledProps = SVGProps<SVGSVGElement> & { size?: number}
+type IconOvalFilledProps = SVGProps<SVGSVGElement>
 
-export const IconOvalFilled = ({ size = 1, ...rest }: IconOvalFilledProps) => {
-  return <svg width={851 * size} height={553 * size} viewBox="0 0 851 553" xmlns="http://www.w3.org/2000/svg" {...rest}>
+export const IconOvalFilled = ({ ...rest }: IconOvalFilledProps) => {
+  return <svg width={851} height={553} viewBox="0 0 851 553" xmlns="http://www.w3.org/2000/svg" {...rest}>
   <rect id="Rectangle_7" data-name="Rectangle 7" width="851" height="553" rx="276.5" fill="#f48987"/>
 </svg>
 }
